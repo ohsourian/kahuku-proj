48ff52ed70be074feac83924188429d416c5dd6b
ohsourian <ohsourian.mgr@gmail.com>
2022-08-30 00:10:30 +0900

    install required package

---
+++ backend/storage/build/ecosystem.config.js
@@ -0,0 +1,11 @@
+/* ecosystem.config.js */
+module.exports = {
+  apps: [
+    {
+      name: "app",
+      script: "./app.js",
+      instances: 0,
+      exec_mode: "cluster",
+    },
+  ],
+};
---
+++ database/mysql/config/init.sql
@@ -0,0 +1,5 @@
+create table if not exists cat
+(
+    id   int unsigned not null auto_increment primary key,
+    name varchar(255) not null
+);
